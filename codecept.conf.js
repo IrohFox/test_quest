a137c0250ec0672b9fcb9f0b109b46624b00798e
Sergey <sklyuchnikov@plotpad.ru>
2020-03-20 16:46:18 +0300

    add new file

---
+++ codecept.conf.js
@@ -0,0 +1,33 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+const { setWindowSize } = require('@codeceptjs/configure');
+setWindowSize(1600, 1200);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'test_quest',
+  translation: 'ru-RU',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
---
+++ link_checking_test.js
@@ -0,0 +1,6 @@
+Feature('link_checking');
+
+Scenario('Софт для бысторого создания скриншотов', (I) => {
+    I.amOnPage('http://blog.csssr.ru/qa-engineer/');
+    I.wait(10);
+});
